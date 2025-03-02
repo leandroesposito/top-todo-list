fff72584e58436ab5f76ffeebd2ed1de49e44dd4
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-02 19:16:00 -0300

    Implement ProjectsDatabaseManager

--- src/ProjectsDatabaseManager.js
+++ src/ProjectsDatabaseManager.js
@@ -1,3 +1,36 @@
-export default class ProjectsDatabaseManager {
+import Project from "./Project.js";
 
+export default class ProjectsDatabaseManager {
+    /**
+     * LocalStorage structure
+     * key        | value
+     * ------------------------------------------------------
+     * username   | [idProject1, idProject2, ..., idProjectN]
+     *            |
+     * idProject1 | {project}
+     * idProjectN | {project}
+     */
+
+    static saveProject(user, project) {
+        const userProjects = JSON.parse(localStorage.getItem(user));
+        userProjects.push(project.id);
+        localStorage.setItem(user, JSON.stringify(userProjects));
+
+        localStorage.setItem(project.id, JSON.stringify(project));
+    }
+
+    static loadProject(id) {
+        return Project.fromJSON(localStorage.getItem(id))
+    }
+
+    static getUserProjects(user) {
+        const userProjects = JSON.parse(localStorage.getItem(user));
+        const projects = [];
+
+        for (const projectId of userProjects) {
+            projects.push(ProjectsDatabaseManager.loadProject(projectId));
+        }
+
+        return projects;
+    }
 }
