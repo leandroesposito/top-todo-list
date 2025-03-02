9e86c21064e87791ba3bddc2f363571529dfc0d8
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-02 20:04:49 -0300

    Implement ProjectsManager

--- src/ProjectsManager.js
+++ src/ProjectsManager.js
@@ -1,3 +1,36 @@
-export default class ProjectsManager {
+import ProjectsDatabaseManager from "./ProjectsDatabaseManager";
 
+export default class ProjectsManager {
+    constructor(user) {
+        this.user = user;
+        this.projects = ProjectsDatabaseManager.getUserProjects(user);
+        this.currenProject = projects[0];
+    }
+
+    addProject(project) {
+        this.projects.push(project);
+        this.currenProject = project;
+        this.saveProject(project);
+    }
+
+    saveProject(project) {
+        ProjectsDatabaseManager.saveProject(this.user, project)
+    }
+
+    addTask(task) {
+        this.currenProject.addTask(task);
+        this.saveProject(this.currenProject);
+    }
+
+    addSubtask(taskIndex, subtask) {
+        const task = this.currenProject.tasks[taskIndex];
+        task.addSubtask(subtask);
+        this.saveProject(this.currenProject);
+    }
+
+    setCurrentProject(projectIndex) {
+        this.currenProject = this.projects[projectIndex];
+
+        return this.currenProject;
+    }
 }
