e233385b0c85c230f9ecf6bdfd7366ae062bd69c
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-05 20:22:05 -0300

    Load new project form as default

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import createNewProjectForm from "./HtmlCreators/newProjectForm.js";
+
+const mainContainer = document.querySelector(".main-container");
+
+mainContainer.innerHTML = "";
+mainContainer.appendChild(createNewProjectForm());
