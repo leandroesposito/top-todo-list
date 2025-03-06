b59aed4451e3198c6d80de0580f4fb868deb2bd9
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-06 18:56:45 -0300

    Add project creation funcionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import "./style.css";
 import createNewProjectForm from "./HtmlCreators/newProjectForm.js";
 import createNewTaskForm from "./HtmlCreators/newTaskForm.js";
+import ProjectsManager from "./ProjectsManager.js";
+import Project from "./Project.js";
 
+const loadContent = (function () {
+    function setContent(creatorFunction) {
+        mainContainer.innerHTML = "";
+        mainContainer.appendChild(creatorFunction());
+    }
 
-const loadContent = function (creatorFunction) {
+    function handleCreateProjectForm(event) {
+        event.preventDefault();
+
+        const form = document.querySelector("form");
+        const formData = new FormData(form);
+
+        const projectTitle = formData.get("project-title");
+        const projectDescription = formData.get("project-description");
+
+        const project = new Project(projectTitle, projectDescription);
+        projectManager.addProject(project);
+    }
+
+    function loadProjectCreation() {
+        setContent(createNewProjectForm);
+
+        const createProjectButton = document.querySelector("form .create-project");
+        createProjectButton.addEventListener("click", handleCreateProjectForm);
+    }
+
+    const userName = "User 1";
+    const userNameContainer = document.querySelector(".user-name");
+    userNameContainer.textContent = userName;
+
     const mainContainer = document.querySelector(".main-container");
 
-    mainContainer.innerHTML = "";
-    mainContainer.appendChild(creatorFunction());
-};
+    const projectManager = new ProjectsManager(userName);
+
+    const createProjectButton = document.querySelector(".sidebar .create-project");
+    createProjectButton.addEventListener("click", loadProjectCreation);
+})();
 
 // loadContent(createNewProjectForm);
 // loadContent(createNewTaskForm);
