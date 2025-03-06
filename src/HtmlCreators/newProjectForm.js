e2e78ddea818348085e9d1bda17a04d06f35e4c5
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-03-06 17:32:52 -0300

    Move functions to utils.js

--- src/HtmlCreators/newProjectForm.js
+++ src/HtmlCreators/newProjectForm.js
@@ -1,72 +1,15 @@
-function createElement(tagName, ...classList) {
-    const element = document.createElement(tagName);
-
-    for (const className of classList) {
-        element.classList.add(className);
-    }
-
-    return element;
-}
-
-function createFormRow() {
-    return createElement("div", "form-row");
-}
-
-function createMainTitle(text) {
-    const titleDiv = createElement("div", "main-title");
-    const icon = createElement("div", "title-icon", "icon");
-    const textNode = document.createTextNode(text);
-    titleDiv.appendChild(icon);
-    titleDiv.appendChild(textNode);
-
-    return titleDiv;
-}
-
-function createLabelInput(data) {
-    const label = document.createElement("label");
-    for (const className of data.labelClassList) {
-        label.classList.add(className);
-    }
-    label.for = data.id;
-    label.textContent = data.label;
-
-    let input;
-    if (data.type === "textarea") {
-        input = document.createElement("textarea");
-    }
-    else {
-        input = document.createElement("input");
-        input.type = data.type;
-    }
-    input.id = data.id;
-    input.name = data.id;
-
-    return [label, input];
-}
-
-function createAddButton(text) {
-    const button = createElement("button", "create-project", "add-button");
-    const plusIcon = createElement("span", "plus-icon", "icon");
-    plusIcon.textContent = "+";
-
-    const textNode = document.createTextNode(text);
-
-    button.appendChild(plusIcon);
-    button.appendChild(textNode);
-
-    return button;
-}
+import * as utils from "./utils.js"
 
 export default function createNewProjectForm() {
     const form = document.createElement("form");
     form.classList.add("content");
 
 
-    const projectTitleDiv = createMainTitle("New Project");
+    const projectTitleDiv = utils.createMainTitle("New Project");
     form.appendChild(projectTitleDiv);
 
-    const titleRow = createFormRow();
-    const [titleLabel, titleInput] = createLabelInput({
+    const titleRow = utils.createFormRow();
+    const [titleLabel, titleInput] = utils.createLabelInput({
         label: "Project title",
         id: "project-title",
         labelClassList: ["section-title"],
@@ -76,8 +19,8 @@ export default function createNewProjectForm() {
     titleRow.appendChild(titleInput);
     form.appendChild(titleRow);
 
-    const descriptionRow = createFormRow();
-    const [descriptionLabel, descriptionInput] = createLabelInput({
+    const descriptionRow = utils.createFormRow();
+    const [descriptionLabel, descriptionInput] = utils.createLabelInput({
         label: "Project description",
         id: "project-description",
         labelClassList: ["section-title"],
@@ -87,9 +30,8 @@ export default function createNewProjectForm() {
     descriptionRow.appendChild(descriptionInput);
     form.appendChild(descriptionRow);
 
-
-    const buttonContainer = createElement("div", "buttons");
-    const createButton = createAddButton("Create Project");
+    const buttonContainer = utils.createElement("div", "buttons");
+    const createButton = utils.createAddButton("Create Project", "create-project");
     buttonContainer.appendChild(createButton);
     form.appendChild(buttonContainer);
 
